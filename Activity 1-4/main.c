beb450a416e24827fe3dedbbfdd9802772f23d1e
99003164 <75420607+99003164@users.noreply.github.com>
2021-01-03 13:20:52 +0530

    Add files via upload

---
+++ Activity 1-4/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int variable1 = 10, variable2 = 20;
+	int sum;
+	sum = variable1 + variable2;
+	printf("The sum of the two variable is: %d",sum);
+	return 0;
+}
